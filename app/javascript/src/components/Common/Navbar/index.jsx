9bde2503362cb16c629d3535b14485b9e34083b6
Pai026 <abhirampai1999@gmail.com>
2021-10-12 20:31:53 +0530

    added sidebar, menubar and card in note listing page

--- app/javascript/src/components/Common/Navbar/index.jsx
+++ app/javascript/src/components/Common/Navbar/index.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 
+import { UserCircle, NeetoInsights, Keyboard } from "neetoicons";
 import { Toastr } from "neetoui";
+import { Sidebar } from "neetoui/v2/layouts";
 import { withRouter } from "react-router-dom";
 
 import authenticationApi from "apis/authentication";
 import { resetAuthTokens } from "apis/axios";
 import { useAuthDispatch } from "contexts/auth";
+import { useUserState } from "contexts/user";
 
-import AccountDropdown from "./AccountDropdown";
-import NavItem from "./NavItem";
+// import AccountDropdown from "./AccountDropdown";
+// import NavItem from "./NavItem";
 
 const NavBar = () => {
+  const { user } = useUserState();
+  const contact = user
+    ? { name: `${user.first_name} ${user.last_name}`, email: `${user.email}` }
+    : null;
   const authDispatch = useAuthDispatch();
   const handleLogout = async () => {
     try {
@@ -24,29 +31,62 @@ const NavBar = () => {
   };
 
   return (
-    <div className="bg-gray-100 nh-sidebar" key="sidebar">
-      <div className="nh-logo">
-        <div className="flex items-center justify-center w-8 h-8 rounded-md">
-          <i className="text-purple-500 ri-flashlight-fill ri-2x" />
-        </div>
-      </div>
-      <div className="flex flex-col items-center justify-between w-full h-full">
-        <div className="flex flex-col items-center justify-start w-full pt-4">
-          <NavItem title="Notes" link="/notes" icon="ri-file-text-line" />
-          <NavItem
-            title="Settings"
-            link="/settings"
-            icon="ri-settings-2-line"
-            subLinks={[
-              { title: "Change password", link: "/my/password/edit" },
-              { title: "My Profile", link: "/my/profile" }
-            ]}
-          />
-        </div>
-        <div className="mb-4">
-          <AccountDropdown handleLogout={handleLogout} />
-        </div>
-      </div>
+    <div className="flex flex-row items-start justify-start">
+      <Sidebar
+        isCollapsed={true}
+        navLinks={[
+          {
+            icon: function noRefCheck() {
+              return <Keyboard size={24} />;
+            },
+            label: "Notes",
+            to: "/notes"
+          },
+          {
+            icon: function noRefCheck() {
+              return <UserCircle size={24} />;
+            },
+            label: "Contact",
+            to: "/contact"
+          },
+          {
+            icon: function noRefCheck() {
+              return <NeetoInsights size={24} />;
+            },
+            items: [
+              {
+                label: "Change Password",
+                to: "/form-elements/input"
+              },
+              {
+                label: "Preferences",
+                to: "/form-elements/select"
+              },
+              {
+                label: "Settings",
+                to: "/form-elements/Toggles"
+              }
+            ],
+            label: "Settings",
+            to: "/form-elements"
+          }
+        ]}
+        profileInfo={{
+          dropdownProps: [
+            {
+              label: "Edit",
+              onClick: null
+            },
+            {
+              label: "Logout",
+              onClick: handleLogout
+            }
+          ],
+          email: `${contact.email}`,
+          imageUrl: "https://randomuser.me/api/portraits/men/10.jpg",
+          name: `${contact.name}`
+        }}
+      />
     </div>
   );
 };
