38a98bc2d6e617475c23b61daee5f056e9649648
Pai026 <abhirampai1999@gmail.com>
2021-10-18 12:12:18 +0530

    removed required from input field

--- app/javascript/src/components/Dashboard/Contacts/NewContactPane.jsx
+++ app/javascript/src/components/Dashboard/Contacts/NewContactPane.jsx
@@ -37,13 +37,11 @@ export default function NewContactPane({ showPane, setShowPane, addNote }) {
                     label="First Name"
                     name="firstName"
                     placeholder="Enter first name"
-                    required={true}
                   />
                   <Input
                     label="Last Name"
                     name="lastName"
                     placeholder="Enter last name"
-                    required={true}
                   />
                 </div>
                 <Input
@@ -51,12 +49,10 @@ export default function NewContactPane({ showPane, setShowPane, addNote }) {
                   name="email"
                   placeholder="Enter email"
                   size={"large"}
-                  required={true}
                 />
                 <Select
                   isClearable
                   isSearchable
-                  required={true}
                   label="Assigned Contact"
                   name="role"
                   options={ROLE_OPTIONS}
